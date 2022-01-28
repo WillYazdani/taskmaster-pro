1a4460f9ec9a60b720183cd02a958e4237d62159
Will Yazdani <yazd0006@gmail.com>
2022-01-28 11:46:37 -0600

    added momentjs functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,6 +13,7 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -43,6 +44,26 @@ var loadTasks = function() {
 
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var auditTask = function(taskEl) {
+  var date = $(taskEl)
+  .find("span")
+  .text()
+  .trim();
+
+  var time = moment(date, "L")
+  .set("hour",17);
+
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger")
+
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger")
+  }
+
+  else if (Math.abs(moment().diff(time, "days"))<=2){
+    $(taskEl).addClass("list-group-item-warning");
+  }
 };
 
 // modal was triggered
@@ -77,6 +98,11 @@ $("#task-form-modal .btn-primary").click(function() {
 
     saveTasks();
   }
+});
+
+//date picker
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // task text was clicked
@@ -135,12 +161,19 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   $(this).replaceWith(dateInput);
 
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      $(this).trigger("change");
+    }
+  });
+
   // automatically bring up the calendar
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   var date = $(this)
   .val()
   .trim()
@@ -165,6 +198,8 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .text(date);
 
     $(this).replaceWith(taskSpan);
+
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // remove all tasks
